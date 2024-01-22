672279b73dacadb74a9edcdb09f1c84e3d9b1089
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-22 15:43:43 -0600

    course 90-92

---
+++ components/header.js
@@ -0,0 +1,8 @@
+export default function header() {
+  return (
+    <>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+    </>
+  );
+}
